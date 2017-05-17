0157af0dc3dade2ee178ca21160ee4db990a3f6c
Bhargav Akula <Bhargav Akula>
2017-05-16 23:59:50 -0400

    initial commit

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,12 +1,14 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-		@RequestMapping("/")
+		//@RequestMapping("/")
+		@RequestMapping(value = "/", method=RequestMethod.GET)
 		public String message(){
-			return "KAKA NAMSTHERY NU KERAKU";
+			return "KAKA NAMSTHERY NU KERAKU new message";
 		}
 }
